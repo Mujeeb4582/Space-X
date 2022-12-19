f0db4b9acae8b6ee28415bd5f829c096a03581d3
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-12-19 21:01:01 +0500

    Add the tailwindCSS dependancies

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      scale: {
+        75: '1.75',
+      },
+    },
+  },
+  plugins: [],
+};
