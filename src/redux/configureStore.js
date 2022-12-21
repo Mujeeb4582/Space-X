1ad4873a9bce9f8f28266f58b422fdf72a11ee06
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-12-21 14:48:39 +0500

    Add the middleware

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import rocketsReducer from './rocketReducer';
 
 const store = configureStore({
   reducer: {
     rockets: rocketsReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
