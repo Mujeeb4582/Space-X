bf22a090b8ef7e819aa1eb6738d9d84d96cb99a9
kennankole <kennankole@gmail.com>
2022-12-22 18:07:58 +0300

    Add snapshot test for Mission component.

--- src/__test__/mission.test.js
+++ src/__test__/mission.test.js
@@ -1,1 +1,22 @@
-// file for mission test
+import React from 'react';
+import { cleanup, render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from '../redux/configureStore';
+import MissionsPage from '../routes/mission';
+
+afterEach(cleanup);
+describe('Missions page component', () => {
+  test('Renders Missions component', () => {
+    const tree = render(
+      <React.StrictMode>
+        <Provider store={store}>
+          <BrowserRouter>
+            <MissionsPage />
+          </BrowserRouter>
+        </Provider>
+      </React.StrictMode>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
