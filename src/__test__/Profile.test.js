75bd7079a5667084705b3cf20ced80f07a9e12e1
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-12-22 16:16:02 +0500

    Add the test for Profile section

--- src/__test__/Profile.test.js
+++ src/__test__/Profile.test.js
@@ -1,1 +1,15 @@
 // file for profile test
+import TestRenderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
+import Profile from '../routes/profile';
+
+describe('Test Profile component', () => {
+  it('should renders Profile component', () => {
+    const ProfileComponent = TestRenderer.create(
+      <Provider store={store}><Router><Profile /></Router></Provider>,
+    ).toJSON();
+    expect(ProfileComponent).toMatchSnapshot();
+  });
+});
