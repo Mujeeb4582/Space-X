e1308f2a46a6f710c389da037d1e859b4df1a2b6
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-12-22 16:16:42 +0500

    Add the test for rocket section

--- src/__test__/rockets.test.js
+++ src/__test__/rockets.test.js
@@ -4,27 +4,39 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../redux/configureStore';
 import Rocket from '../components/rocket';
+import Rockets from '../routes/rockets';
 
-test('renders correctly', () => {
-  const data = {
-    id: 1,
-    rocket_name: 'Falcon 1',
-    description: 'A very fast ship',
-    flickr_images: ['img'],
-  };
+describe('Rockets', () => {
+  it('Rockets component is rendered correctly', () => {
+    const rockets = render(
+      <Provider store={store}>
+        <Rockets />
+      </Provider>,
+    );
+    expect(rockets).toMatchSnapshot();
+  });
 
-  const { getByText } = render(
-    <Provider store={store}>
-      <Rocket
-        key={data.id}
-        rocketName={data.rocket_name}
-        imgURL={data.flickr_images}
-        description={data.description}
-        reserved={!!data.reserved}
-        id={data.id}
-      />
-    </Provider>,
-  );
+  test('renders correctly', () => {
+    const data = {
+      id: 1,
+      rocket_name: 'Falcon 1',
+      description: 'A very fast ship',
+      flickr_images: 'img',
+    };
 
-  expect(getByText(/Reserve/i)).toBeInTheDocument();
+    const { getByText } = render(
+      <Provider store={store}>
+        <Rocket
+          key={data.id}
+          rocketName={data.rocket_name}
+          imgURL={data.flickr_images}
+          description={data.description}
+          reserved={!!data.reserved}
+          id={data.id}
+        />
+      </Provider>,
+    );
+
+    expect(getByText(/Reserve/i)).toBeInTheDocument();
+  });
 });
